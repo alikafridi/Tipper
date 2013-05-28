46af625b9f48ae0618448db867524314165de2ca
Ali Afridi <afridi2@illinois.edu>
2013-05-28 14:55:25 -0400

    folder

---
+++ gen/com/afridi2/Tipper/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.afridi2.Tipper;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/afridi2/Tipper/TipCalculator.java
@@ -0,0 +1,108 @@
+package com.afridi2.Tipper;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.widget.EditText;
+
+public class TipCalculator extends Activity {
+
+	private static final String currentBill = "bill_amount"; //bill without tip
+	private static final String currentTipPercent = "tip_percent"; //percent of tip to give
+	private static final String currentTipAmount = "tip_amount"; //value of tip
+	private static final String currentFinalBill = "final_bill"; //total amount of bill
+	
+	private double billAmount;
+	private double tipPercent;
+	private double tipAmount;
+	private double finalBill;
+	
+	EditText billAmountET;
+	EditText tipPercentET;
+	EditText tipAmountET;
+	EditText finalBillET;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tip_calculator);
+		
+		if (savedInstanceState == null){
+			billAmount = 0.0;
+			tipPercent = 15.0;
+			tipAmount = 0.0;
+			finalBill = 0.0;
+		} 
+		else {
+			billAmount = savedInstanceState.getDouble(currentBill);
+			tipPercent = savedInstanceState.getDouble(currentTipPercent);
+			tipAmount = savedInstanceState.getDouble(currentTipAmount);
+			finalBill = savedInstanceState.getDouble(currentFinalBill);			
+		}
+		
+		billAmountET = (EditText) findViewById(R.id.billAmountEdit);
+		tipPercentET = (EditText) findViewById(R.id.tipPercentEdit);
+		tipAmountET = (EditText) findViewById(R.id.tipAmountEdit);
+		finalBillET = (EditText) findViewById(R.id.finalAmountEdit);
+		
+		billAmountET.addTextChangedListener(billBeforeTipListener);
+		
+	}
+	
+	private TextWatcher billBeforeTipListener = new TextWatcher(){
+
+		@Override
+		public void afterTextChanged(Editable arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+				int arg3) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+			// TODO Auto-generated method stub
+			try {
+				billAmount = Double.parseDouble(s.toString());
+			}
+			catch(NumberFormatException e) {
+				billAmount = 0.0;
+			}
+			update();
+		}
+	};
+	
+	private void update() {
+		double tipPerc = Double.parseDouble(tipPercentET.getText().toString());
+		double finalTip = billAmount*tipPerc/100;
+		double finBill = billAmount + finalTip;
+		tipAmountET.setText(String.format("%.02f",finalTip));
+		finalBillET.setText(String.format("%.02f",finBill));
+	}
+	
+	protected void onSaveInstanceState(Bundle outState){
+		
+		super.onSaveInstanceState(outState);
+		outState.putDouble(currentBill, billAmount);
+		outState.putDouble(currentTipPercent, tipPercent);
+		outState.putDouble(currentTipAmount, tipAmount);
+		outState.putDouble(currentFinalBill, finalBill);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_tip_calculator, menu);
+		return true;
+	}
+
+}
